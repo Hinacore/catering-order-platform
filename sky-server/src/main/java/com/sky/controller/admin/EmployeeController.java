5c081c9e2933017775b6d008935b01cd50bb04bb
Hina <2579249584@qq.com>
2026-07-03 11:36:08 +0800

    员工模块启用禁用功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -86,13 +86,31 @@ public class EmployeeController {
         return Result.success();
     }
 
-
+    /**
+     * 员工分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
     @GetMapping("/page")
     @ApiOperation("员工分页查询")
     public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO) {
         log.info("员工分页查询:{}", employeePageQueryDTO);
-
         PageResult pageResult= employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
+
+    /**
+     * 启用/停用员工
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用/停用员工")
+    public Result startOrStop(@PathVariable Integer status,Long id) {
+        log.info("启用/停用员工:{},{}", status,id);
+        employeeService.startOrStop(status,id);
+        return Result.success();
+    }
+
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -106,4 +106,18 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(total, records);
     }
 
+    /**
+     * 启用/停用员工
+     * @param status
+     * @param id
+     */
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        //update employee set status = #{status} where id = #{id}
+        Employee employee = new Employee();
+        employee.setId(id);
+        employee.setStatus(status);
+        employeeMapper.update(employee);
+    }
+
 }
